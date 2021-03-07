4ef192c30092e1c5f0ae02d45b0ef33a26d0078d
David Hambraeus <Ddhambraeus@gmail.com>
2021-03-07 12:55:19 +0100

    Remove class BZ, getBrillouinZonePoints is now a function
    Also made some improvements, decreasing the number of points to check
    based on how large the circles are. There is no need to check
    points further than some k_u as all circles with a diameter larger than
    some value has more than n points. k_u should grow as sqrt(n)

--- Brillouin.py
+++ Brillouin.py
@@ -1,148 +1,225 @@
 import numpy as np
 import math
 
-class BZ:
-    def __init__(self, v2=[0, 1], n=3):
-        """
-        v2 is the second basis vector. The first is always [1, 0]
+def getBrillouinZonePoints(v=[0, 1], n=3):
+    """
+    getBrillouinZonePoints returns a dictionary with integers from
+    1 to n (inclusive) as keys and lists of points, sorted by angle
+    to positive x-axis, as values. The list of points with key i are 
+    the outer boundry of the i:th Brillouin zone.
+    The inner boundry of the i:th Brillouin zone would, of course, be
+    the outer boundry of the i-1:th Brillouin zone.
 
-        We assume that v2 is such that there was no smaller v2 that would result in the same lattice.
-        """
-        self.tol = 1e-8
-        self.v1 = np.array([1, 0])
-        self.v2 = np.array(v2)
-        self.toLatticeCoords = np.linalg.inv(np.column_stack((self.v1, self.v2)))
-        self.bzones = dict()
-        self.n = n
+    The argument v specifies the second primitive vector of the lattice.
+    The first is always taken to be [1, 0].
+    """
 
-        self.latticePoints = []
-        for i in range(-self.n, self.n+1):
-            for j in range(-self.n, self.n+1):
-                if not i == j == 0:
-                    self.latticePoints.append(i * self.v1 + j * self.v2)
+    # Set the tolerance
+    tol = 1e-10
 
-        print(f"Calculated latticePoints: length {len(self.latticePoints)}")
-        # 
-        circleCenters = []
-        seenCenters = set()
-        for i, p in enumerate(self.latticePoints[:-1]):
-            if i % 100 == 0:
-                print(i)
-            for q in self.latticePoints[i+1:]:
-                # If q is a multiple of p
-                if np.abs(np.cross(p, q)) < self.tol:
-                    continue
+    # Set the primitive basis vectors. 
+    # The vector v is transformed to have positive y and x between -0.5 and 0.5.
+    u = np.array([1, 0])
+    v = np.array(v)
+    if v[1] < 0:
+        v = -v
+    v[0] = v[0] - math.floor(v[0] + 0.5)
 
-                center = self.findCircleCenter(p, q)
 
-                # If the circle obviously contains more than n points
-                # The largest rectangle that fits in a circle of radius r
-                # is a square with sidelength a = 2 * r / sqrt(2). 
-                # The smalles number of points inside such a rectangle is
-                # floor(a / v2_y) * floor(a / v1_x) 
-                a = np.linalg.norm(center) * np.sqrt(2)
-                if np.floor(a / self.v2[1]) * np.floor(a) > self.n:
-                    continue
+    # bzones will be the return value
+    bzones = dict()
 
-                centerTuple = tuple(np.around(center, decimals=8))
-                if centerTuple in seenCenters:
-                    continue
-                #for seenCenter in circleCenters:
-                    #if np.allclose(seenCenter, center):
-                        #break
+    # If we are only interested in circles containing up to n points,
+    # there is no need to check lattice points beyond some k_u u coordinate
+    # and k_v v coordinate.
+    # A circle touching u coordinate k_u has diameter at least k_u
+    # and thus a s = k_u / sqrt(2) sided square fits inside it.
+    # This square contains at least floor(s / v_y) * floor(s / u_x)
+    # points.
+    # Thus, floor(s / v_y) * floor(s / u_x) < n
+    # => (s / v_y - 1) * (s / u_x - 1) < n
+    # => s^2 - s*v_y - s*u_x + v_y*u_x < n
+    # => s < (v_y + u_x) / 2 + sqrt(((v_y + u_x) / 2)^2 + n - v_y * u_x
+    # => k_u = (v_y + u_x) / sqrt(2) + sqrt((v_y + u_x)^2 / 2 + 2 n - 2 v_y*u_x)
+    k_u = int((v[1] + 1)/math.sqrt(2) + math.sqrt((v[1] + 1)**2 / 2 +
+                                                        2 * n -
+                                                        2 * v[1]))
+    # Likewise for k_v but with s = k_v * |v| / sqrt(2) gives
+    # k_v = 1/|v| * ((v_y + u_x) / sqrt(2) + sqrt((v_y + u_x)^2 / 2 + 2 n - 2 v_y*u_x))
+    k_v = int(((v[1] + 1)/math.sqrt(2) +
+                    math.sqrt((v[1] + 1)**2 / 2 +
+                              2 * n - 2 * v[1])) / np.linalg.norm(v))
 
+    print(f"k_u: {k_u}, k_v: {k_v}")
+
+    # To avoid considering the same circle multiple times we'll
+    # keep a dictionary with all points seen so far as key and
+    # sets of the points thet they have been on the edge of a
+    # circle with as the keys.
+    seenWith = dict()
+
+    # We can choose the first point to have positive u-coordinate 
+    # and smaller absolute value of u-coordinate, or equal absolute 
+    # value of u coordinate and smaller or equal absolute value of v-coordinate
+    # The first because the circle centers stemming from the other case
+    # are just minus the ones from this case. The second to avoid counting
+    # the pair of points p, q AND q, p, which of course yields the same circle.
+    for a1 in range(k_u + 1):
+        for b1 in range(-k_v, k_v+1):
+
+            p = a1 * u + b1 * v
+            itera2 = list(range(-k_u, -a1 + 1)) + list(range(a1, k_u+1))
+
+            for a2 in itera2:
+
+                if abs(a2)==a1:
+                    iterb2 = list(range(-k_v, -abs(b1) + 1)) + list(range(abs(b1), k_v+1)) 
                 else:
-                    seenCenters.add(centerTuple)
-                    seenCenters.add((-centerTuple[0], -centerTuple[1]))
-                    circleCenters.append(center)
-                    circleCenters.append(-center)
+                    iterb2 = range(-k_v, k_v+1)
 
-        print(len(circleCenters))
-        #for center in filter(lambda c: np.linalg.norm(c) < self.n/2, circleCenters):
-        for center in circleCenters:
-            pointsInside, pointsOnEdge = self.latticePointsInCircle(center)
-            for zone in range(pointsInside + 1, pointsInside + pointsOnEdge):
-                if zone in self.bzones:
-                    self.bzones[zone].append(center)
-                else:
-                    self.bzones[zone] = [center]
+                for b2 in iterb2:
 
-    def latticePointsInCircle(self, center):
-        """
-        Return the number of lattice points inside the circle
-        and the number of lattice points on the edge
-        """
-        # First, find the smallest parallellogram enclosing the circle
-        radius = np.linalg.norm(center)
-        if radius > self.n:
-            return -1
+                    #if a1==1 and b1==-1 and a2==-1 and b2==-2:
+                        #breakpoint()
 
-        # Calculate a1 bounds
-        # The distance from the line a1 * v1 + lambda * v2 to the center point
-        # is | v2 x (a1 * v1 - center) | / |v2|
-        # where x denotes crossproduct.
-        # We thus want to solve
-        # | v2 x (a1 * v1 - center) | / |v2| < radius
-        # for the smallest (in abs value) integer a1 
-        # | a1 * v2 x v1 - v2 x center | < radius * |v2|
-        # If rightside positive
-        # a1 * v2 x v1 - v2 x center < radius * |v2|
-        # a1 * v2 x v1 < radius * |v2| + v2 x center
-        # a1 > (radius * |v2| + v2 x center) / v2 x v1     
-        #   inequality switch as v2 x v1 is negative
-        # If rightside negative
-        # v2 x center - a1 * v2 x v1 < radius * |v2|
-        # a1 * v2 x v1 > v2 x center - radiuss * |v2|
-        # a1 < (v2 x center - radius * |v2|) / v2 x v1
-        # Once again, the inequality switches because v2 x v1 is negative
-        a1Lo = (np.cross(self.v2, center) + (radius + self.tol) * np.linalg.norm(self.v2)) / np.cross(self.v2, self.v1)
-        a1Hi = (np.cross(self.v2, center) - (radius + self.tol) * np.linalg.norm(self.v2)) / np.cross(self.v2, self.v1)
+                    # If this q has already been in a circle with p
+                    # The center has already been added and there is no need
+                    # to do it again.
+                    if (a1, b1) in seenWith and (a2, b2) in seenWith[(a1, b1)]:
+                        continue
+                    q = a2 * u + b2 * v
 
-        # Calculate a1 bounds
-        # The distance from the line a2 * v2 + lambda * v1 to the center point
-        # is | v1 x (a2 * v2 - center) | / |v1|
-        # Like before
-        # | v1 x (a2 * v2 - center) | < radius * |v1|
-        # If positive
-        # a2 v1 x v2 < radius * |v1| + v1 x center
-        # a2 < (radius * |v1| + v2 x center) / v1 x v2
-        # If negative
-        # a2 v1 x v2 > (v1 x center - radius * |v1|)
-        # a2 > (v1 x center - radius * |v1|) / v1 x v2
-        a2Lo = (np.cross(self.v1, center) - (radius + self.tol) * np.linalg.norm(self.v1)) / np.cross(self.v1, self.v2)
-        a2Hi = (np.cross(self.v1, center) + (radius + self.tol) * np.linalg.norm(self.v1)) / np.cross(self.v1, self.v2)
+                    # If p and q are parallel, that would be no circle
+                    if np.abs(np.cross(p, q)) < tol:
+                        continue
 
-        # These are the most extreme bounds. Since the lattice coordinates are integers,
-        # We can ceil / floor them accordingly. 
-        a1Lo = math.ceil(a1Lo)
-        a2Lo = math.ceil(a2Lo)
-        a1Hi = math.floor(a1Hi)
-        a2Hi = math.floor(a2Hi)
+                    # Calculate the circle center
+                    center = findCircleCenter(p, q)
 
-        # For each latticePoint in this parallellogram, check if it is inside the circle
-        # If so, increment insideCount
-        # If instead it is on the boundry, increment edgeCount
-        insideCount = 0
-        edgeCount = 0
-        for a1 in range(a1Lo, a1Hi + 1):
-            for a2 in range(a2Lo, a2Hi + 1):
-                d = np.linalg.norm(a1 * self.v1 + a2 * self.v2 - center)
-                if d < radius - self.tol:
-                    insideCount += 1
-                elif abs(d - radius) < self.tol:
-                    edgeCount += 1
+                    # If the circle obviously contains more than n points, skip it
+                    # The largest rectangle that fits in a circle of radius r
+                    # is a square with sidelength a = 2 * r / sqrt(2). 
+                    # The smalles number of points inside such a square is
+                    # floor(a / v_y) * floor(a / u_x) 
+                    a = np.linalg.norm(center) * np.sqrt(2)
+                    if np.floor(a / v[1]) * np.floor(a) > n:
+                        continue
 
-        return insideCount, edgeCount
+                    # Calculate the number of points inside the circle and 
+                    # a list of the points on the edge of the circle.
+                    noPointsInside, pointsOnEdge = latticePointsInCircle(u, v, center, tol)
 
-        
-    def findCircleCenter(self, p, q):
-        A = 2 * np.row_stack((p,q))
-        b = np.array([np.dot(p, p), np.dot(q, q)])
-        return np.linalg.solve(A, b)
+                    # If the number of points inside is greater than n, skip it
+                    if noPointsInside + 1 > n:
+                        continue
 
 
-    def getBrillouinZone(self, n):
-        return sorted(self.bzones[n], key=angle)
+                    # For each point on the edge, at all the other points
+                    # to it's set of points it's been seen with.
+                    # Also add the negative versions.
+                    for point1 in pointsOnEdge:
+
+                        mpoint1 = (-point1[0], -point1[1])
+
+                        if point1 not in seenWith:
+                            seenWith[point1] = set()
+                            seenWith[mpoint1] = set()
+
+                        for point2 in pointsOnEdge:
+                            if point1 != point2:
+                                mpoint2 = (-point2[0], -point2[1])
+                                seenWith[point1].add(point2)
+                                seenWith[mpoint1].add(mpoint2)
+
+
+                    # Now we get to the good stuff :)
+                    # The center of a circle containing k points with j points on the edge
+                    # is an outer corner of the k:th, k+1:th, ..., k+j-1:th zone
+                    # and an inner corner of the k+j:th zone.
+                    for zone in range(noPointsInside + 1, noPointsInside + len(pointsOnEdge)):
+                        if zone in bzones:
+                            bzones[zone].append(center)
+                            bzones[zone].append(-center)
+                        else:
+                            bzones[zone] = [center, -center]
+
+    # bzones are popluated, now we sort them.
+    for zone in bzones:
+        bzones[zone].sort(key=angle)
+
+    # And return the dict
+    return bzones
+
+def latticePointsInCircle(u, v, center, tol=1e-8):
+    """
+    Return the number of lattice points inside the circle
+    and a list of the lattice coordinates of the points on the edge
+    """
+    # First, find the smallest parallellogram enclosing the circle
+    radius = np.linalg.norm(center)
+
+    # Calculate a bounds
+    # The distance from the line a * u + lambda * v to the center point
+    # is | v x (a * u - center) | / |v|
+    # where x denotes crossproduct.
+    # We thus want to solve
+    # | v x (a * u - center) | / |v| < radius
+    # for the smallest (in abs value) integer a 
+    # | a * v x u - v x center | < radius * |v|
+    # If rightside positive
+    # a * v x u - v x center < radius * |v|
+    # a * v x u < radius * |v| + v x center
+    # a > (radius * |v| + v x center) / v x u     
+    #   inequality switch as v x u is negative
+    # If rightside negative
+    # v x center - a * v x u < radius * |v|
+    # a * v x u > v x center - radiuss * |v|
+    # a < (v x center - radius * |v|) / v x u
+    # Once again, the inequality switches because v x u is negative
+    aLo = (np.cross(v, center) + (radius + tol) * np.linalg.norm(v)) / np.cross(v, u)
+    aHi = (np.cross(v, center) - (radius + tol) * np.linalg.norm(v)) / np.cross(v, u)
+
+    # Calculate b bounds
+    # The distance from the line b * v + lambda * u to the center point
+    # is | u x (b * v - center) | / |u|
+    # Like before
+    # | u x (b * v - center) | < radius * |u|
+    # If positive
+    # b u x v < radius * |u| + u x center
+    # b < (radius * |u| + v x center) / u x v
+    # If negative
+    # b u x v > (u x center - radius * |u|)
+    # b > (u x center - radius * |u|) / u x v
+    bLo = (np.cross(u, center) - (radius + tol) * np.linalg.norm(u)) / np.cross(u, v)
+    bHi = (np.cross(u, center) + (radius + tol) * np.linalg.norm(u)) / np.cross(u, v)
+
+    # These are the most extreme bounds. Since the lattice coordinates are integers,
+    # We can ceil / floor them accordingly. 
+    aLo = math.ceil(aLo)
+    bLo = math.ceil(bLo)
+    aHi = math.floor(aHi)
+    bHi = math.floor(bHi)
+
+    # For each latticePoint in this parallellogram, check if it is inside the circle
+    # If so, increment insideCount
+    # If instead it is on the boundry, increment edgeCount
+    insideCount = 0
+    edgePoints = []
+    for a in range(aLo, aHi + 1):
+        for b in range(bLo, bHi + 1):
+            d = np.linalg.norm(a * u + b * v - center)
+            if d < radius - tol:
+                insideCount += 1
+            elif abs(d - radius) < tol:
+                edgePoints.append((a, b))
+
+    return insideCount, edgePoints
+
+
+def findCircleCenter(p, q):
+    A = 2 * np.row_stack((p,q))
+    b = np.array([np.dot(p, p), np.dot(q, q)])
+    return np.linalg.solve(A, b)
 
 def angle(point):
     return np.arctan2(point[1], point[0])
--- main.py
+++ main.py
@@ -1,33 +1,86 @@
-import Brillouin as B
-
 import time
-
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+
+import Brillouin as B
 
 
-start = time.time()
+############################
+#   User variables
+############################
 
+# Set the number of brillouin zones to plot
+# A value of 80 takes about 70 seconds on my machine,
+# 20 take about 4 seconds
 n = 20
-bz = B.BZ([0.4,0.8], n)
-#bz = B.BZ([0.5,np.sqrt(3)/2], n)
 
-end = time.time()
-print(f"Time taken: {end - start}")
+# Set the second primitive basis vector.
+# The first is always [1,0].
+# If you want some other first primitive basis vector,
+# the author suggests you tilt your head and zoom in
 
-patches = []
+for x in np.linspace(-0.5,0.5,21):
+    v = [x, math.sqrt(3)/2]
 
-for i in range(n, 0, -1):
-    patches.append(Polygon(bz.getBrillouinZone(i)))
 
-pc = PatchCollection(patches, alpha=1)
-colors = np.linspace(0,1,len(patches))
-pc.set_array(colors)
+    ############################
+    #   Calculation of points
+    ############################
 
-fig, ax = plt.subplots()
-ax.add_collection(pc)
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
-plt.show()
+    start = time.time()
+
+    bz = B.getBrillouinZonePoints(v, n)
+
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
+    ############################
+    #   Plotting
+    ############################
+
+    plt.style.use('seaborn-darkgrid')
+
+    patches = []
+
+    # Plot "backwards" so the later zones do not cover the earlier
+    for i in range(n, 0, -1):
+        patches.append(Polygon(bz[i]))
+
+    pc = PatchCollection(patches, alpha=1)
+
+    # Construct the colors
+    cmap = plt.get_cmap("tab20b")
+    colors = cmap(np.tile(np.linspace(0,1,20), (n-1) // 20 + 1))
+    pc.set_color(colors[::-1])
+
+    # Create the figure and axis
+    fig, ax = plt.subplots()
+
+    # Add the collection of polygons
+    ax.add_collection(pc)
+
+    # Set limits
+    xmax = max([p[0] for p in bz[n]])
+    ymax = max([p[1] for p in bz[n]])
+    ax.set_xlim(-xmax, xmax)
+    ax.set_ylim(-ymax, ymax)
+
+    # Make aspect ratio 1:1
+    ax.axis('equal')
+
+    # Make axis fill figure
+    ax.set_position([0, 0, 1, 1])
+
+    # Set ticks to the integers
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Show the plot
+    #plt.show()
+
+    # Uncomment to save the figure to disk
+    plt.savefig(f"brillouin_{v[0]:.2f}-{v[1]:.2f}.pdf")
